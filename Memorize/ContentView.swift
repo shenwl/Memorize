d6db8e67dffc2d69ddecac8c2557867006dc23a4
shenwl <shenweilun96@gmail.com>
2020-05-25 20:49:43 +0800

    Initial Commit

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by shenwl on 2020/5/25.
+//  Copyright © 2020 shenwl. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
