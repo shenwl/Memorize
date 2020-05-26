37aee97a9dd4b55bd965b3ca604f903a90532eb5
shenwl <shenweilun96@gmail.com>
2020-05-26 21:08:47 +0800

    init

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -10,12 +10,35 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        // HStack的content参数直接放在外面
+        HStack {
+            // 函数第二个参数，被大括号包裹的，可以省略label，甚至可以放在函数外面
+            // ForEach(0..<4, content: {...})
+            // 把content内容放在外面
+            ForEach(0..<4) {
+                index in CardView(isFaceUp: false)
+            }
+        }
+        .padding().foregroundColor(Color.orange).font(Font.largeTitle)
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct CardView: View {
+    // 支持翻转展示
+    var isFaceUp: Bool;
+    
+    var body: some View {
+        ZStack {
+            if(isFaceUp) {
+                RoundedRectangle(cornerRadius: 10.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 10.0).stroke(lineWidth: 3)
+                Text("👻")
+            } else {
+                RoundedRectangle(cornerRadius: 10.0).fill()
+            }
+           
+        }
+        
     }
 }
+
